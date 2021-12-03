c8c2a64a154d7aea6d0583448e59558950a1efee
Chijith Jerin <chijithjerin2004@gmail.com>
2021-12-03 19:30:46 +0530

    new update

---
+++ Black-Jack-game/app.js
@@ -0,0 +1,19 @@
+let firstCard = 11;
+let secondCard = 10;
+let hasBlackJack = false;
+let isaAlive = true;
+let sum = firstCard + secondCard;
+let message = " ";
+
+if (sum < 21)
+{
+    console.log("Do you want to draw a new card ? 🙂");
+    isaAlive = true;
+}
+
+else if (sum === 21)
+{
+    console.log("WoooHooo!! You've got Blackjack!! 🤑🤑");
+    hasBlackJack = true;
+}
+
